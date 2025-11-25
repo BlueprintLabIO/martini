6ebfade4da0357a399e5f720075b952a231e2ced
YFanSh1011 <89020588+YFanSh1011@users.noreply.github.com>
2025-11-25 21:24:31 +1100

    Added analytics

--- @martini-kit/demos/src/routes/+layout.svelte
+++ @martini-kit/demos/src/routes/+layout.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.png';
+	import posthog from 'posthog.js';
 
 	let { children } = $props();
+	
+    posthog.init(
+      'phc_OGNJlDrkfSloaice291LvwuaHbN2AkAjD0HS15Gaqa1',
+      {
+        api_host: 'https://us.i.posthog.com',
+        defaults: '2025-05-24',
+        person_profiles: 'always', 
+      }
+    )
 </script>
 
 <svelte:head>
