2f03f506a0eb7279bec3acfd08e89cf439e32703
Yao Ke <yaoke259@outlook.com>
2025-11-25 21:33:10 +1100

    new docs

--- @martini-kit/demos/src/routes/+layout.svelte
+++ @martini-kit/demos/src/routes/+layout.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.png';
-	import posthog from 'posthog.js';
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
 
 	let { children } = $props();
 	
-    posthog.init(
-      'phc_OGNJlDrkfSloaice291LvwuaHbN2AkAjD0HS15Gaqa1',
-      {
-        api_host: 'https://us.i.posthog.com',
-        defaults: '2025-05-24',
-        person_profiles: 'always', 
-      }
-    )
+    if (browser) {
+        posthog.init(
+            'phc_OGNJlDrkfSloaice291LvwuaHbN2AkAjD0HS15Gaqa1',
+            {
+                api_host: 'https://us.i.posthog.com',
+                defaults: '2025-05-24',
+                person_profiles: 'always',
+            }
+        );
+    }
 </script>
 
 <svelte:head>
@@ -20,4 +23,3 @@
 </svelte:head>
 
 {@render children()}
-
